13d095d738a1919b07901476b85eedfb50392a3f
Elmer Danilo Hernandez <eldh.gt@gmail.com>
2023-04-18 16:37:13 -0600

    creando category module

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LayoutComponent } from './components/layout/layout.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
@@ -25,8 +24,8 @@ const routes: Routes = [
         component: HomeComponent
       },
       {
-        path: 'category/:id',
-        component: CategoryComponent
+        path: 'category',
+        loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule)
       },
       {
         path: 'product/:id',
--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -5,9 +5,9 @@ import { WebsiteRoutingModule } from './website-routing.module';
 
 import { SwiperModule } from 'swiper/angular';
 import { SharedModule } from '../shared/shared.module';
+
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { LayoutComponent } from './components/layout/layout.component';
 
@@ -15,7 +15,6 @@ import { LayoutComponent } from './components/layout/layout.component';
   declarations: [
     NavComponent,
     HomeComponent,
-    CategoryComponent,
     ProductDetailComponent,
     LayoutComponent
   ],
