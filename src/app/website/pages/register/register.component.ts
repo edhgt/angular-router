1d808ca068747742bf51361071f0ee107b09bc5c
Danilo Hernandez <eldh.gt@gmail.com>
2023-04-18 22:33:08 -0600

    guard para onExit del componente

--- src/app/website/pages/register/register.component.ts
+++ src/app/website/pages/register/register.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { OnExit } from 'src/app/guards/exit.guard';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
-
+export class RegisterComponent implements OnExit{
+  onExit() {
+    const rta = confirm('Logic from component. Are you sure to exit this page?');
+    return rta;
+  }
 }
